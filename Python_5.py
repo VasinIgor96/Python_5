19974231484788f773a5461b652fda26406246f0
Vasin Igor <vasin.igor0796@gmail.net>
2023-04-05 20:24:32 +0300

    Ex_4 Task done

--- Python_5.py
+++ Python_5.py
@@ -1,1 +1,6 @@
-
+def common_elements(list1, list2):
+    return list(set(list1) and set(list2))
+list1 = [1, 2, 3, 4, 5]
+list2 = [3, 4, 5, 6, 1]
+common = common_elements(list1, list2)
+print(common)
