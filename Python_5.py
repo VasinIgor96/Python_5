170c34a6a27acb7e2bff6ab9ad84ee20c709f3c5
Vasin Igor <vasin.igor0796@gmail.net>
2023-04-05 19:35:31 +0300

    Ex_1 Task done

--- Python_5.py
+++ Python_5.py
@@ -1,1 +1,11 @@
-
+import random
+nams = list(range(-10,20))
+even_nums = []
+odd_nums = []
+for num in nams:
+     if num %2 == 0:
+         even_nums.append(num)
+     else:
+         odd_nums.append(num)
+print("Even numbers: ",even_nums)
+print("Odd numbers: ",odd_nums)
