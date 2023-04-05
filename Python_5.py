c5108e14f17288fe838055c6b8f6f442c22f6fc3
Vasin Igor <vasin.igor0796@gmail.net>
2023-04-05 19:48:15 +0300

    Ex_2 Task done

--- Python_5.py
+++ Python_5.py
@@ -1,1 +1,8 @@
+numbers = [1, 2, 3, 2, 4, 5, 1, 6, 7, 6, 8, 9, 6, 1, 2]
 
+unique_numbers = []
+for num in numbers:
+    if num not in unique_numbers:
+        unique_numbers.append(num)
+
+print(unique_numbers)
