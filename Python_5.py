b30f183849928f6ebc8f3c1adcba436f70cdbf8f
Vasin Igor <vasin.igor0796@gmail.net>
2023-04-05 19:57:42 +0300

    Ex_3 Task done

--- Python_5.py
+++ Python_5.py
@@ -1,1 +1,5 @@
-
+def sort_words_by_length(words):
+    return sorted(words, key=len)
+words = ["Яблука","Полуниця", "Банани", "Апельсини", "Ківі","Фрукти"]
+sorted_words = sort_words_by_length(words)
+print(sorted_words)
