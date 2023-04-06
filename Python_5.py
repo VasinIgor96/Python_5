051372a3cbeb2decc68e993569738b44c066fa88
Vasin Igor <vasin.igor0796@gmail.net>
2023-04-06 17:46:05 +0300

    Ex_7 Task done

--- Python_5.py
+++ Python_5.py
@@ -1,1 +1,18 @@
+import random
+numbers = list(range(10,20))
+min_num = numbers[0]
+max_num = numbers[0]
 
+for num in numbers:
+    if num < min_num:
+        min_num = num
+    elif num > max_num:
+        max_num = num
+
+min_index = numbers.index(min_num)
+max_index = numbers.index(max_num)
+
+numbers[min_index] = max_num
+numbers[max_index] = min_num
+
+print(numbers)
