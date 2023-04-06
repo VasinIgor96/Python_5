fe2b24e2ef776cf1a4cc9757b54593254b45b5f8
Vasin Igor <vasin.igor0796@gmail.net>
2023-04-06 18:20:16 +0300

    Ex_10 Task done

--- Python_5.py
+++ Python_5.py
@@ -1,1 +1,10 @@
 
+strings = ["Огірок", "Слива", "Помідор", "Салат", "Груша", "Суниця"]
+prefix = "С"
+result = []
+
+for i in strings:
+    if i.startswith(prefix):
+        result.append(i)
+
+print(result)
