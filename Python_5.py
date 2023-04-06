ef93c277300ff3ea0b55116f59fd26a6e11bfa85
Vasin Igor <vasin.igor0796@gmail.net>
2023-04-06 17:09:38 +0300

    Ex_5 Task done

--- Python_5.py
+++ Python_5.py
@@ -1,1 +1,11 @@
-
+import random
+numbers = list(range(-10,10))
+def positive_numbers(numbers):
+    print(numbers)
+    positive_nums = []
+    for num in numbers:
+        if num > 0:
+            positive_nums.append(num)
+    return positive_nums
+positive_nums_list = positive_numbers(numbers)
+print(positive_nums_list)
